dfa11ac6d7ca3af485ad977f7fbfc548b50ee95f
Elias Mueggler <elias.mueggler@gmail.com>
2016-11-11 15:59:56 +0100

    no library, just header files

--- cmake/sophus-extras.cmake
+++ cmake/sophus-extras.cmake
@@ -1,4 +1,1 @@
-# This overrides the dependency tracker with the sophus library file.
-set(@PROJECT_NAME@_LIBRARIES
-  @CATKIN_DEVEL_PREFIX@/lib/sophus${CMAKE_SHARED_LIBRARY_SUFFIX})
 set(@PROJECT_NAME@_INCLUDE_DIR @CATKIN_DEVEL_PREFIX@/include)
